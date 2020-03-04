0c594f33a6df9a409ae289e90e2d33dc3836d5c9
lennyhuang(黄磊) <lennyhuang@tencent.com>
2020-03-05 00:27:55 +0800

    增加对图片文件夹和字体文件夹的兼容

--- gulp-task/gulpfile.babel.js
+++ gulp-task/gulpfile.babel.js
@@ -1,74 +1,93 @@
-'use strict';
-import gulp from 'gulp';
+"use strict";
+import gulp from "gulp";
 /*======= 文件删除 ======*/
-import del from 'del';
-import child_process from 'child_process';
-import bs from 'browser-sync';
-import { jstask } from './script';
-import { htmltask } from './html';
+import del from "del";
+import child_process from "child_process";
+import bs from "browser-sync";
+import { font, fonttask } from "./font";
+import { img, imgtask } from "./img";
+import { jstask } from "./script";
+import { htmltask } from "./html";
 
 let browserSync = bs.create();
 
 /*======= css ======*/
 function css() {
-    return gulp.src(['../src/css/**/*'])
-        .pipe(gulp.dest('../dev/css'))
-        .pipe(gulp.dest('../public/css'))
-};
+  return gulp
+    .src(["../src/css/**/*"])
+    .pipe(gulp.dest("../dev/css"))
+    .pipe(gulp.dest("../public/css"));
+}
 /*======= lib ======*/
 function lib() {
-    return gulp.src(['../src/lib/**/*'])
-        .pipe(gulp.dest('../dev/lib'))
-        .pipe(gulp.dest('../public/lib'))
-};
-
-/*======= img ======*/
-function img() {
-    return gulp.src(['../src/img/**/*'])
-        .pipe(gulp.dest('../dev/img'))
-        .pipe(gulp.dest('../public/img'))
-};
-
-
+  return gulp
+    .src(["../src/lib/**/*"])
+    .pipe(gulp.dest("../dev/lib"))
+    .pipe(gulp.dest("../public/lib"));
+}
 
 function clean() {
-    return del(['../dev/js/**/*', '../public/js/**/*', '../dev/html/**/*', '../public/html/**/*', '../dev/css/**/*', '../public/css/**/*'], { force: true });
-};
+  return del(
+    [
+      "../dev/font/**/*",
+      "../public/font/**/*",
+      "../dev/img/**/*",
+      "../public/img/**/*",
+      "../dev/js/**/*",
+      "../public/js/**/*",
+      "../dev/html/**/*",
+      "../public/html/**/*",
+      "../dev/css/**/*",
+      "../public/css/**/*"
+    ],
+    { force: true }
+  );
+}
 
 let process,
-    restart = function () {
-        process && process.kill();
-        process = child_process.spawn('gulp.cmd', ['run'], {
-            stdio: 'inherit'
-        });
-    };
-function server(cb) {
-    browserSync.init({
-        server: {
-            baseDir: "../"
-        },
-        startPath: "/dev/html/index.html"
+  restart = function() {
+    process && process.kill();
+    process = child_process.spawn("gulp.cmd", ["run"], {
+      stdio: "inherit"
     });
-    cb();
-};
+  };
+function server(cb) {
+  browserSync.init({
+    server: {
+      baseDir: "../"
+    },
+    startPath: "/dev/html/index.html"
+  });
+  cb();
+}
 function reload(done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 }
 function watch() {
-    gulp.watch('../src/lib/**/*', lib);
-    gulp.watch('../src/css/**/*', css);
-    gulp.watch('../src/img/**/*', img);
-    gulp.watch('../src/js/**/*', gulp.series(htmltask, jstask, reload));
-    gulp.watch('../src/html/**', gulp.series(htmltask, reload));
-    gulp.watch('./*.js', restart);
+  gulp.watch("../src/lib/**/*", lib);
+  gulp.watch("../src/css/**/*", css);
+  gulp.watch("../src/font/**/*", fonttask);
+  gulp.watch("../src/img/**/*", imgtask);
+  gulp.watch("../src/js/**/*", gulp.series(htmltask, jstask, reload));
+  gulp.watch("../src/html/**", gulp.series(htmltask, reload));
+  gulp.watch("./*.js", restart);
 }
 
-exports.run = gulp.series(clean, gulp.parallel(lib, img, css, jstask), htmltask, server, watch, (done) => { done() });
+exports.run = gulp.series(
+  clean,
+  gulp.parallel(lib, font, img, css, jstask),
+  htmltask,
+  server,
+  watch,
+  done => {
+    done();
+  }
+);
 
-/*   
+/*
  * auto restart gulp
  **/
 exports.default = () => {
-    restart();
+  restart();
 };
--- gulp-task/html.js
+++ gulp-task/html.js
@@ -1,26 +1,30 @@
-
-import gulp from 'gulp';
-import del from 'del';
-import fileinclude from 'gulp-file-include';
+import { src, dest, series } from "gulp";
+import del from "del";
+import fileinclude from "gulp-file-include";
 /*======= 文件MD5时间戳 ======*/
-import cachebust from 'gulp-cache-bust';
+import cachebust from "gulp-cache-bust";
 
-function html() {
-    return gulp.src(['../src/html/*.html'])
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(cachebust({
-            type: 'timestamp'
-        }))
-        .pipe(gulp.dest('../dev/html'))
-        .pipe(gulp.dest('../public/html'));
-};
-
-function cleanHtml() {
-    return del(['../dev/html/**/*', '../public/html/**/*'], { force: true });
+function html(cb) {
+  src(["../src/html/*.html"])
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file"
+      })
+    )
+    .pipe(
+      cachebust({
+        type: "timestamp"
+      })
+    )
+    .pipe(dest("../dev/html"))
+    .pipe(dest("../public/html"));
+  cb();
 }
 
-const htmltask = gulp.series(cleanHtml, html);
-export { htmltask }
+function cleanHtml() {
+  return del(["../dev/html/**/*", "../public/html/**/*"], { force: true });
+}
+
+const htmltask = series(cleanHtml, html);
+export { html, htmltask };
